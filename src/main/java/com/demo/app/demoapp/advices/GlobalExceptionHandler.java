351f1f690909d444258b9d9da8cc2c3b1008f7ef
vishnuibs <vishnu.prakkottil.ibs@gmail.com>
2024-11-19 10:13:18 +0530

    Updated Global Exception Handler
    Added Response Class

--- src/main/java/com/demo/app/demoapp/advices/GlobalExceptionHandler.java
+++ src/main/java/com/demo/app/demoapp/advices/GlobalExceptionHandler.java
@@ -1,35 +1,38 @@
 package com.demo.app.demoapp.advices;
 
 import com.demo.app.demoapp.exceptions.ResourceNotFoundException;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 
 @RestControllerAdvice
+@AllArgsConstructor
 public class GlobalExceptionHandler {
 
+    private WebApplicationContext webApplicationContext;
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse<Object>> handleResourceNotFoundException(ResourceNotFoundException exception) {
+    public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException exception) {
 
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .message(exception.getMessage())
                 .build();
-        return buildErrorResponseEntity(apiError);
+        return new ResponseEntity<>(apiError,apiError.getStatus());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<Object>> handleException(Exception exception) {
+    public ResponseEntity<ApiError> handleException(Exception exception) {
 
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .message(exception.getMessage())
                 .build();
-        return buildErrorResponseEntity(apiError);
-    }
-
-    private ResponseEntity<ApiResponse<Object>> buildErrorResponseEntity (ApiError apiError) {
-        return new ResponseEntity<>(new ApiResponse<>(apiError), apiError.getStatus());
+        return new ResponseEntity<>(apiError,apiError.getStatus());
     }
 }
